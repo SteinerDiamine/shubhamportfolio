fe40ebf8f945ebbc1d6cd36658b6fd642cdcd1c8
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-05-31 14:51:11 +0530

    commit

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
+    { name: "Inspiration", link: "#inspiration" },
     { name: "Contact", link: "#contact" },
   ];
   
@@ -104,7 +104,7 @@ export const navItems = [
     },
   ];
   
-  export const testimonials = [
+  export const inspiration = [
     {
       quote:
         "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
